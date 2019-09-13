6b37a834ed6e1747898e6ad0430b102fcdccd86e
Aleksander Eriksen <jakeriksen@gmail.com>
2019-09-13 11:19:53 +0200

    TASK A: according to ex_01

---
+++ src/aleksander_eriksen_ex/ex01/deck_by_comp.py
@@ -0,0 +1,18 @@
+SUITS = ('C', 'S', 'H', 'D')
+VALUES = range(1, 14)
+
+def deck_loop():
+    deck = []
+    for suit in SUITS:
+        for val in VALUES:
+            deck.append((suit, val))
+    return deck
+
+def deck_comp():
+    deck = [(suit, val) for suit in ('C', 'S', 'H', 'D') for val in range(1, 14)]
+    return deck
+
+
+if __name__ == '__main__':
+    if deck_loop() != deck_comp():
+        print('ERROR!')
