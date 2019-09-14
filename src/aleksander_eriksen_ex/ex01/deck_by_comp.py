5d722eef494134ace89196fc6948ebf2fef88eaf
Aleksander Eriksen <jakeriksen@gmail.com>
2019-09-14 18:11:39 +0200

    TASK A: according to ex_01

--- src/aleksander_eriksen_ex/ex01/deck_by_comp.py
+++ src/aleksander_eriksen_ex/ex01/deck_by_comp.py
@@ -1,5 +1,6 @@
-SUITS = ('C', 'S', 'H', 'D')
-VALUES = range(1, 14)
+SUITS = ('C', 'S', 'H', 'D')  # Defining the suits.
+VALUES = range(1, 14)  # Got the values from 1 to 13.
+
 
 def deck_loop():
     deck = []
@@ -8,6 +9,8 @@ def deck_loop():
             deck.append((suit, val))
     return deck
 
+
+# The list comprehension of the code above:
 def deck_comp():
     deck = [(suit, val) for suit in ('C', 'S', 'H', 'D') for val in range(1, 14)]
     return deck
