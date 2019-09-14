de75de2b3ec895e98bd1facaa65681628f978cf4
Aleksander Eriksen <jakeriksen@gmail.com>
2019-09-14 18:11:53 +0200

    TASK B: according to ex_01

--- src/aleksander_eriksen_ex/ex01/comp_to_loop.py
+++ src/aleksander_eriksen_ex/ex01/comp_to_loop.py
@@ -1,19 +1,17 @@
 def squares_by_comp(n):
-    return [k**2 for k in range(n) if k % 3 == 1]
+    return [k ** 2 for k in range(n) if k % 3 == 1]
 
 
-
+# Rewritten the code from list comprehension to a loop
 def squares_by_loop(n):
-    list = []
-    for number in range(n):
-        if number % 3 is 1:
-            list.append(number**2)
-    return list
+    list_ = []
+    for i in range(n):  # Iterates over the range of number you choose
+        if i % 3 is 1:  # Gives a sequence with 3 numbers between. 1, 4, 7, 13... (depending on the range)
+            list_.append(i ** 2)  # Squares the given number and adds it to the list
+    return list_
 
 
 if __name__ == '__main__':
     number = 9
     if squares_by_comp(number) != squares_by_loop(number):
         print('ERROR!')
-
-
