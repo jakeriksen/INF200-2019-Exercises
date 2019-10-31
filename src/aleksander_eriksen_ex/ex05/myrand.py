fdb5a9e3d0d20f3f3bc0cc17dc862ad407528936
Aleksander Eriksen <jakeriksen@gmail.com>
2019-10-31 14:07:20 +0100

    TASK C

--- src/aleksander_eriksen_ex/ex05/myrand.py
+++ src/aleksander_eriksen_ex/ex05/myrand.py
@@ -36,6 +36,22 @@ class LCGRand:
 	def random_sequence(self, length):
 		return RandIter(self, length)
 
+	def infinite_random_sequence(self):
+		"""
+		Generate an infinite sequence of random numbers.
+
+		Yields
+		------
+		int
+			A random number.
+		"""
+		while True:
+			yield self.rand()
+
+
+
+
+
 
 class RandIter:
 	def __init__(self, random_number_generator, length):
@@ -66,7 +82,10 @@ class RandIter:
 		RuntimeError
 			If iter is called twice on the same RandIter object.
 		"""
-		pass
+		if self.num_generated_numbers is not None:
+			raise RuntimeError('RandIter cannot be called twice')
+		self.num_generated_numbers = 0
+		return self
 
 	def __next__(self):
 		"""
@@ -83,5 +102,13 @@ class RandIter:
 			If the ``__next__`` method is called before ``__iter__``.
 		StopIteration
 			If ``self.length`` random numbers are generated.
-		pass
 		"""
+
+		if self.num_generated_numbers is None:
+			raise RuntimeError('__next__ is called before __iter__')
+		if self.length == self.num_generated_numbers:
+			raise StopIteration
+
+		random_num = self.generator.rand()
+		self.num_generated_numbers += 1
+		return random_num
